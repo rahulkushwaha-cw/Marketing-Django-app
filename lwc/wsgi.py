d35e9708b8cc6e404033788781c548114186f5d1
rahulkushwaha12 <kushwaharesonance@gmail.com>
2016-12-29 17:01:40 +0530

    Add Requirements

--- lwc/wsgi.py
+++ lwc/wsgi.py
@@ -12,3 +12,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lwc.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+try:
+	from whitenoise.django import DjangoWhiteNoise
+
+	application = get_wsgi_application()
+	application = DjangoWhiteNoise(application)
+except:
+	pass
